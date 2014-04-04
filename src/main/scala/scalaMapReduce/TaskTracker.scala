55cb198d02df4c3937c767440f4affcf98b0c707
fsi206914 <liang.xjtu@gmail.com>
2014-04-04 15:24:40 -0500

    taskTracker communication conponent

--- src/main/scala/scalaMapReduce/TaskTracker.scala
+++ src/main/scala/scalaMapReduce/TaskTracker.scala
@@ -3,20 +3,79 @@ package scalaMapReduce
 import akka.actor._
 import common._
 import javaMapReduce._;
-import java.util._;
+import scala.collection.mutable._;
 import java.io._;
 
 import scala.collection.mutable.StringBuilder;
 
 
-class TaskTracker(val taskTrackerName: String){
+class TaskTracker(val taskTrackerName: String, val rPort: Int){
   var mapperCounter: Integer = 0; 
   var reducerCounter: Integer = 0; 
 
-  var rPort = 10041
+  val taskStatus = new HashMap[Int, TaskProgress]() with SynchronizedMap[Int, TaskProgress]
+
 
   def getTaskTrackerName()  = taskTrackerName;
   def getRPort() = rPort;
+
+  def runTask(taskInfo: TaskInfo){
+
+    if(taskInfo.getType() == TaskMeta.TaskType.MAPPER){
+      val mapperTaskInfo = taskInfo.asInstanceOf[MapperTaskInfo];
+      mapperCounter.synchronized{
+      if(mapperCounter < TaskTracker.NUM_OF_MAPPER_SLOTS){
+
+        println("task tracker " + getTaskTrackerName()
+          + " received runTask request taskid:" + taskInfo.getTaskID() + " "
+          + taskInfo.getType());
+
+        mapperCounter += 1;
+
+        val args: Array[String] = Array( classOf[MapperWorker].getName(),
+              mapperTaskInfo.getTaskID().toString(), mapperTaskInfo.getInputPath(),
+              mapperTaskInfo.getOffset().toString,
+              mapperTaskInfo.getBlockSize().toString(), mapperTaskInfo.getOutputPath(),
+              mapperTaskInfo.getMapper(), mapperTaskInfo.getPartitioner(),
+              mapperTaskInfo.getInputFormat(), mapperTaskInfo.getReducerNum().toString(),
+              getTaskTrackerName(), getRPort().toString );
+
+          try {
+            Utility.startJavaProcess(args, taskInfo.getJobID());
+          } catch {
+            case e: Exception => e.printStackTrace();
+          }
+
+      }}
+    } else{
+      val reducerTaskInfo = taskInfo.asInstanceOf[ReducerTaskInfo];
+      reducerCounter.synchronized{
+      if(reducerCounter < TaskTracker.NUM_OF_REDUCER_SLOTS){
+
+        println("task tracker " + getTaskTrackerName()
+          + " received runTask request taskid:" + taskInfo.getTaskID() + " "
+          + taskInfo.getType());
+
+        reducerCounter += 1;
+
+        val args = Array( classOf[ReducerWorker].getName(),
+              reducerTaskInfo.getTaskID().toString(), reducerTaskInfo.getOrderId().toString(),
+              reducerTaskInfo.getReducer(),
+              reducerTaskInfo.getOutputFormat(), reducerTaskInfo.getInputPath(),
+              reducerTaskInfo.getOutputPath(), getTaskTrackerName(),
+              getRPort().toString() );
+
+          try {
+            Utility.startJavaProcess(args, taskInfo.getJobID());
+          } catch {
+            case e: Exception => e.printStackTrace();
+          }
+
+      }}
+    }
+
+  }
+
 
 }
 
@@ -31,26 +90,23 @@ object TaskTracker{
   val TASK_MAPPER_OUTPUT_PREFIX = "mapper_output_task_";
   val JOB_CLASSPATH_PREFIX = "job";
   var currentMaxTaskId = (math.random*1000).toInt;
+}
 
-  def getTASK_MAPPER_OUTPUT_PREFIX() = TASK_MAPPER_OUTPUT_PREFIX;
+class TaskStatusChecker(val tt: TaskTracker) extends Runnable {
 
-  val mapTasks: Map[Integer, TaskMeta] = Collections.synchronizedMap(new HashMap[Integer, TaskMeta]());
-  val reduceTasks: Map[Integer, TaskMeta] = Collections.synchronizedMap(new HashMap[Integer, TaskMeta]());
-  val jobs: Map[Integer, JobMeta] = Collections.synchronizedMap(new HashMap[Integer, JobMeta]());
+  val ALIVE_CYCLE = 8000; // milliseconds
 
-
-  val mapTasksQueue = new PriorityQueue[TaskMeta](10,
-
-            new Comparator[TaskMeta]() {
-              def compare(o1: TaskMeta, o2: TaskMeta) = o1.getJobID() - o2.getJobID();
-      });
-
-  val reduceTasksQueue = new PriorityQueue[TaskMeta](10,
-
-            new Comparator[TaskMeta]() {
-              def compare(o1: TaskMeta, o2: TaskMeta) = o1.getJobID() - o2.getJobID();
-      });
-
-  val tasktrackers = Collections.synchronizedMap(new HashMap[String, TaskTrackerMeta]());
-
+  /**
+   * periodically check time stamp and set status
+   */
+  def run(){
+    tt.taskStatus.synchronized{
+      for( (id, taskProgress) <- tt.taskStatus){
+        val curTime = System.currentTimeMillis();
+        if ((curTime - taskProgress.getTimestamp() > ALIVE_CYCLE)
+            && taskProgress.getStatus() != TaskMeta.TaskStatus.SUCCEED)
+                taskProgress.setStatus(TaskMeta.TaskStatus.FAILED);
+      }
+    }
+  }
 }
--- src/main/scala/scalaMapReduce/TaskTrackerActor.scala
+++ src/main/scala/scalaMapReduce/TaskTrackerActor.scala
@@ -5,84 +5,97 @@ import common._
 import javaMapReduce._;
 import java.util._;
 import java.io._;
+import scala.collection.mutable._;
 
 import scala.collection.mutable.StringBuilder
 
 object TaskTrackerApp extends App  {
 //  Http / "rapture.io" / "welcome.txt" > File / "home" / "liang" / "welcome.txt"
 //  File / "home" / "liang" / "welcome.txt" > Socket("localhost", 6789)
-
-  val system = ActorSystem("TaskTracker1System", Config.JobTrackerConfig)
-  val jtActor = system.actorOf(Props(new TaskTrackerActor(new TaskTracker("tt1"))) , name = "JobTrackerActor")
+  override def main(args: Array[String]){
+    val ttName = args(0);
+    val rPort = args(1).toInt;
+    val system = ActorSystem("TaskTracker" + ttName + "System", Config.TaskTracker1Config)
+    val ttActor = system.actorOf(Props(new TaskTrackerActor(new TaskTracker("tt" + ttName, rPort))) , name = "TaskTracker"+ ttName)
+    ttActor ! Start
+  }
 }
 
 
 class TaskTrackerActor(val tt: TaskTracker) extends Actor {
+
+  import context._
+  val remote = context.actorSelection("akka.tcp://JobTrackerSystem@127.0.0.1:10021/user/JobTrackerActor")
   def receive = {
+
+    case Start =>
+      println("TaskTrackerActor begin working")
+ //     run();
     case "RequestJobID" =>
       println("JobTracker received message RequestJobID")
       sender ! 123
     case taskInfo: TaskInfo => 
       println("TaskTrackerActor received an object taskInfo");
-      runTask(taskInfo);
+      tt.runTask(taskInfo);
     case _ => 
       println("JobTracker got something unexpected.")
   }
 
-  def runTask(taskInfo: TaskInfo){
 
-    if(taskInfo.getType() == TaskMeta.TaskType.MAPPER){
-      val mapperTaskInfo = taskInfo.asInstanceOf[MapperTaskInfo];
-      tt.mapperCounter.synchronized{
-      if(tt.mapperCounter < TaskTracker.NUM_OF_MAPPER_SLOTS){
+  /* start the task status checker */
+  def run(){
+    import java.util.concurrent.ScheduledExecutorService;
+    import java.util.concurrent.Executors;
+    import java.util.concurrent.ScheduledFuture;
+    import java.util.concurrent.TimeUnit;
 
-        println("task tracker " + tt.getTaskTrackerName()
-          + " received runTask request taskid:" + taskInfo.getTaskID() + " "
-          + taskInfo.getType());
+    val taskStatusChecker = new Thread( new TaskStatusChecker(tt));
 
-        tt.mapperCounter += 1;
 
-        val args: Array[String] = Array( classOf[MapperWorker].getName(),
-              mapperTaskInfo.getTaskID().toString(), mapperTaskInfo.getInputPath(),
-              mapperTaskInfo.getOffset().toString,
-              mapperTaskInfo.getBlockSize().toString(), mapperTaskInfo.getOutputPath(),
-              mapperTaskInfo.getMapper(), mapperTaskInfo.getPartitioner(),
-              mapperTaskInfo.getInputFormat(), mapperTaskInfo.getReducerNum().toString(),
-              tt.getTaskTrackerName(), tt.getRPort().toString );
+    taskStatusChecker.setDaemon(true);
+    val schExec = Executors.newScheduledThreadPool(8);
+    val schFutureChecker = schExec.scheduleAtFixedRate(taskStatusChecker, 0,
+            2, TimeUnit.SECONDS);
 
-          try {
-            Utility.startJavaProcess(args, taskInfo.getJobID());
-          } catch {
-            case e: Exception => e.printStackTrace();
+
+    val taskStatusUpdater = new Thread( new Runnable(){
+
+      def run(){
+        val toDelete = new ListBuffer[Int]();
+        val taskStatus = tt.taskStatus;
+        taskStatus.synchronized{
+          
+          /* 
+           * In order to reuse TaskTrackerUpdatePkg source class,
+           * We have to use ArrayList to represent taskList.
+           */
+          val taskList = new java.util.ArrayList[TaskProgress]()
+          for(tp <- taskStatus.values) taskList.add(tp);
+
+
+          for( (id, taskProgress) <- taskStatus)
+            if (taskProgress.getStatus() != TaskMeta.TaskStatus.INPROGRESS){
+                toDelete += id;
+
+                if(taskProgress.getType() == TaskMeta.TaskType.MAPPER)
+                  tt.mapperCounter.synchronized{ tt.mapperCounter -= 1;}
+                else tt.reducerCounter.synchronized{ tt.reducerCounter -= 1;}
+            }
+
+          for (id<- toDelete) taskStatus.remove(id);
+
+          tt.mapperCounter.synchronized{
+            tt.reducerCounter.synchronized{
+              val pkg = new TaskTrackerUpdatePkg(tt.taskTrackerName, TaskTracker.NUM_OF_MAPPER_SLOTS - tt.mapperCounter,
+                        TaskTracker.NUM_OF_REDUCER_SLOTS - tt.reducerCounter, "12",
+                        taskList, 123);
+              remote ! pkg;
+            }
           }
+        }
+      }
 
-      }}
-    } else{
-      val reducerTaskInfo = taskInfo.asInstanceOf[ReducerTaskInfo];
-      tt.reducerCounter.synchronized{
-      if(tt.reducerCounter < TaskTracker.NUM_OF_REDUCER_SLOTS){
-
-        println("task tracker " + tt.getTaskTrackerName()
-          + " received runTask request taskid:" + taskInfo.getTaskID() + " "
-          + taskInfo.getType());
-
-        tt.reducerCounter += 1;
-
-        val args = Array( classOf[ReducerWorker].getName(),
-              reducerTaskInfo.getTaskID().toString(), reducerTaskInfo.getOrderId().toString(),
-              reducerTaskInfo.getReducer(),
-              reducerTaskInfo.getOutputFormat(), reducerTaskInfo.getInputPath(),
-              reducerTaskInfo.getOutputPath(), tt.getTaskTrackerName(),
-              tt.getRPort().toString() );
-
-          try {
-            Utility.startJavaProcess(args, taskInfo.getJobID());
-          } catch {
-            case e: Exception => e.printStackTrace();
-          }
-
-      }}
-    }
+      });
 
   }
 
